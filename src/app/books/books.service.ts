01fe4da281759f4e037623198eed0444ef47287d
velasco <velascoperezd@gmail.com>
2021-12-09 07:22:23 +0100

    add book changes

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -18,15 +18,15 @@ export class BooksService {
   constructor(private http: HttpClient) {}
 
   getBooks(
-    libroPorPagina: number,
-    paginaActual: number,
+    booksByPage: number,
+    actualPage: number,
     sort: string,
     sortDirection: string,
     filterValue: any
   ): void {
     const request = {
-      pageSize: libroPorPagina,
-      page: paginaActual,
+      pageSize: booksByPage,
+      page: actualPage,
       sort,
       sortDirection,
       filterValue,
