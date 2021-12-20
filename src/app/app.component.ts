c2f230788c27406072494575e375de415905ce95
velasco <velascoperezd@gmail.com>
2021-12-20 22:51:15 +0100

    change chat position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { SecurityService } from './seguridad/security.service';
 
 @Component({
@@ -6,11 +7,23 @@ import { SecurityService } from './seguridad/security.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  userState!: boolean;
+  userSubscription!: Subscription;
   abrirMenu = false;
   constructor(private securityService: SecurityService) {}
 
   ngOnInit(): void {
     this.securityService.loadUser();
+    this.userSubscription = this.securityService.securityChange.subscribe(
+      (status) => {
+        this.userState = status;
+      }
+    );
   }
+
+  ngOnDestroy(){
+    this.userSubscription.unsubscribe()
+  }
+
 }
